35a60a56ebbdf2333c606e4e1efd60acf5e226ac
DaviiiidGL <david.giraldo52@eia.edu.co>
2026-08-07 23:00:47 -0500

    pom.xml and MainApp.java update dependencies

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module co.eia.camusic.camusic {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens co.eia.camusic.camusic to javafx.fxml;
-    exports co.eia.camusic.camusic;
-}
